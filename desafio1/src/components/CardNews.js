42574e2219f2908c3b43d9b17313bb73ac918888
Flavio Milani <frmilanij@gmail.com>
2024-06-07 09:25:34 -0300

    Criando components - Criando props e styles

--- desafio1/src/components/CardNews.js
+++ desafio1/src/components/CardNews.js
@@ -18,16 +18,23 @@ e
         cardLeft.setAttribute("class", "card__left");
         
         const author = document.createElement("span");
+        author.textContent = "por " + (this.getAttribute("author") || "Anônimo");
         const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("url-link");
+        linkTitle.target = "_blank";
         const noticeContent = document.createElement("p");
+        noticeContent.textContent = this.getAttribute("content");
 
         cardLeft.appendChild(author);
         cardLeft.appendChild(linkTitle);
         cardLeft.appendChild(noticeContent);
 
         const cardRight = document.createElement("div"); 
-        cardRight.setAttribute("class", "card__right");
+        cardRight.setAttribute("class", "card__right"); 
+
         const noticeImage = document.createElement("img");
+        noticeImage.src = this.getAttribute("notice-img") || "assets/image/default-image.webp";
 
         cardRight.appendChild(noticeImage);
 
@@ -38,7 +45,59 @@ e
     }
 
     styles() {
+        const style = document.createElement("style");
+        style.textContent = 
 
+        `   
+        .card {
+    display: flex;
+    flex-direction: row;
+    width: 70%;
+    height: 250px;
+    -webkit-box-shadow: 0px 0px 50px -2px #000000bf;
+    -moz-box-shadow: 0px 0px 50px -2px #000000bf;
+    box-shadow: 0px 0px 50px -2px #000000bf;
+    border-radius: 1rem;
+    justify-content: space-between;
+    margin: 100px auto;
+}
+
+.card__left {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 10px;
+}
+
+.card__left > span {
+    font-weight: 400;
+}
+
+.card__left > a {
+    margin-top: 10px;
+    font-size: 25px;
+    font-weight: bold;
+    text-decoration: none;
+    color: #000000
+}
+
+.card__left > p {
+    color: #565656;
+}
+
+.card__right {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card__right > img {
+    padding-right: 20px;
+    height: 80%;
+}
+        `
+        
+        return style;
     }
 }
 
