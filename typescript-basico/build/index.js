389b8986b5a7d70469384bac69e11f272790f5fe
Flavio Milani <frmilanij@gmail.com>
2024-06-07 20:10:52 -0300

    Trabalhando com tipos de variaveis

---
+++ typescript-basico/build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+//Variaveis
+//Tipos Primitivos: boolean, number, string;
+let ligado = true; //or false.
+let nome = "Maria";
+let idade = 2024;
+let altura = 1.79; //Number aceita casa decimal.
+//Tipos Especiais: null or undefined
+let nulo = null; //Recebe apenas null.
+let indefinido = undefined; //Recebe apenas undefined.
+//Tipos Abrangentes: any or void.
+let retornoVazio; //Não recebe nada, apenas "nada"
+let qualquerCoisa = "LITERALMENTE QUALQUER COISA"; //Posso colocar um camelo aqui, que vai dar bom.
---
+++ typescript-basico/src/index.ts
@@ -0,0 +1,17 @@
+
+//Variaveis
+ 
+
+    //Tipos Primitivos: boolean, number, string;
+    let ligado : boolean = true; //or false.
+    let nome : string = "Maria";
+    let idade : number = 2024;
+    let altura : number = 1.79; //Number aceita casa decimal.
+
+    //Tipos Especiais: null or undefined
+    let nulo : null = null; //Recebe apenas null.
+    let indefinido : undefined = undefined; //Recebe apenas undefined.
+
+    //Tipos Abrangentes: any or void.
+    let retornoVazio : void; //Não recebe nada, apenas "nada"
+    let qualquerCoisa : any = "LITERALMENTE QUALQUER COISA"; //Posso colocar um camelo aqui, que vai dar bom.
